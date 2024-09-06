8d04e10e096d76855341579cfd44e639c8b41297
shubhanshubb <shubhanshubb@gmail.com>
2024-09-06 20:05:25 +0530

    UI

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,47 +1,21 @@
-import React from 'react';
-import './nav.css';
-import { BiHomeSmile, BiUser, BiBook, BiMessageSquareDetail } from 'react-icons/bi';
-import { useState } from 'react';
+import React from 'react'
+import './nav.css'
+import { BiHomeSmile} from 'react-icons/bi'
+import { BiUser} from 'react-icons/bi'
+import { BiBook} from 'react-icons/bi'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import { useState } from 'react'
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#');
-
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <button 
-        role="link" 
-        onClick={() => setActiveNav('#')} 
-        className={activeNav === '#' ? 'active' : ''}
-        aria-label="Home"
-      >
-        <BiHomeSmile />
-      </button>
-      <button 
-        role="link" 
-        onClick={() => setActiveNav('#about')} 
-        className={activeNav === '#about' ? 'active' : ''}
-        aria-label="About"
-      >
-        <BiUser />
-      </button>
-      <button 
-        role="link" 
-        onClick={() => setActiveNav('#experience')} 
-        className={activeNav === '#experience' ? 'active' : ''}
-        aria-label="Experience"
-      >
-        <BiBook />
-      </button>
-      <button 
-        role="link" 
-        onClick={() => setActiveNav('#contact')} 
-        className={activeNav === '#contact' ? 'active' : ''}
-        aria-label="Contact"
-      >
-        <BiMessageSquareDetail />
-      </button>
+      <button href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeSmile/></button>
+      <button href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUser/></button>
+      <button href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></button>
+      <button href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></button>
     </nav>
-  );
-};
+  )
+}
 
-export default Nav;
+export default Nav
