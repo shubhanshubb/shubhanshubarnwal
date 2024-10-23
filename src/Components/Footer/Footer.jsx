fe6ff91a0ef30a990ee0dff3459ae951c3bba180
shubhanshubb <shubhanshubb@gmail.com>
2024-10-23 14:13:13 +0530

    Responsive Mobile

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,12 +12,12 @@ const Footer = () => {
         <img src={SB_Tech} alt="tech" />
       </div>
 
-      <ul className="permalinks">
+      {/* <ul className="permalinks">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#contact">Contact</a></li>
-      </ul>
+      </ul> */}
 
       <div className="footer_socials">
         <a href="https://facebook.com/MeShubhanshu" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
